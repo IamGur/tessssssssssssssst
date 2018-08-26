7fad0a47b76154ac88521c5da4133a45cf3bd12d
IamGur <36259923+IamGur@users.noreply.github.com>
2018-08-26 19:08:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,104 @@
-{
+const Discord = require('discord.js');
+const YTDL = require("ytdl-core");
+const client = new Discord.Client();
+const Status = `${prefix}help `;
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+    client.channels.get(logchannel).send(`**Bot Logged in as ${client.user.tag}\, ${client.guilds.size} Servers \, ${client.users.size} Users Dav-ID:${Dav} !** `);
+    client.user.setPresence({ game: { name: `${Status}`, url: 'https://twitch.tv/....', type: 1 } });
+});
+
+function play(connection, message) {
+    var server = servers[message.guild.id];
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {
+        filter: "audioonly"
+    }));
+
+    server.queue.shift();
+
+    server.dispatcher.on("end", function() {
+        if (server.queue[0]) play(connection, message);
+        else connection.disconnect();
+    });
 
 }
+var servers = {};
+var prefix = '!';
+client.on("message", async message => {
+    var args = message.content.substring(prefix.length).split(" ");
+    if (!message.content.startsWith(prefix)) return;
+    switch (args[0].toLowerCase()) {
+        case "play":
+            if (!message.guild.member(client.user).hasPermission('SPEAK')) return message.channel.send('**Sorry, but i cant join/speak in this channel!**').catch(console.error);
+            if (!args[1]) {
+                message.channel.send("**Please provide a URL YouTube link to me to play song.**");
+                return;
+            }
+
+            if (!message.member.voiceChannel) {
+                message.channel.send("**I think it may work better if you are in a voice channel!**");
+                return;
+            }
+
+            if (console.error) {
+                message.channel.send("**Sorry, but i cant search videos in YouTube! Provide a link to play!**");
+                return;
+            }
+
+            if (!servers[message.guild.id]) servers[message.guild.id] = {
+                queue: []
+            };
+
+            var server = servers[message.guild.id];
+
+            server.queue.push(args[1]);
+
+            message.channel.sendMessage('``You song has been added to the queue.``')
+            if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+                play(connection, message);
+            });
+            break;
+        case "stop":
+            var server = servers[message.guild.id];
+            if (!message.member.voiceChannel) {
+                message.channel.send("**I think it may work better if you are in a voice channel!**");
+                return;
+            }
+
+            if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+            message.channel.send('``The queue of songs removed.``');
+            break;
+        case "skip":
+            if (!message.member.voiceChannel) {
+                message.channel.send("**I think it may work better if you are in a voice channel!**");
+                return;
+            }
+
+            var server = servers[message.guild.id];
+            if (server.dispatcher) server.dispatcher.end();
+            message.channel.send('``The song has been sucessfully skipped.``');
+            break;
+        case "pause":
+            if (!message.member.voiceChannel) {
+                message.channel.send("**I think it may work better if you are in a voice channel!**");
+                return;
+            }
+
+            var server = servers[message.guild.id];
+            if (server.dispatcher) server.dispatcher.pause();
+            message.channel.send('``The song is paused.``');
+            break;
+        case "resume":
+            if (!message.member.voiceChannel) {
+                message.channel.send("**I think it may work better if you are in a voice channel!**");
+                return;
+            }
+
+            var server = servers[message.guild.id];
+            if (server.dispatcher) server.dispatcher.resume();
+            message.channel.send('``The song is sucessfully continued.``');
+            break;
+    }
+});
+client.login(process.env.BOTTOKEN);
